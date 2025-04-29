d16bdee783065dd137a9d9009b57752766d117f3
H Usama Tanveer <usamatanveer526@gmail.com>
2025-04-29 13:34:16 -0500

    Create 01_extract_data.py

---
+++ code/1_data_preprocessing/01_extract_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Extract data from Excel datasets for meta-analysis.
+"""
+
+import pandas as pd
+import os
+
+# Define paths
+DATA_DIR = os.path.join('..', '..', 'data')
+EXCEL_DIR = os.path.join(DATA_DIR, 'excel')
+RAW_DIR = os.path.join(DATA_DIR, 'raw')
+
+def main():
+    """Main function to extract data from Excel files."""
+    # TODO: Implement data extraction
+    print("Data extraction script")
+
+if __name__ == "__main__":
+    main()
